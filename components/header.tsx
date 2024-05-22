fdb4757c8ab0af7c60783b29efc88addeeedb780
gold <huangjin@webull.com>
2024-05-22 23:41:59 +0800

    feat: adjust header css

--- components/header.tsx
+++ components/header.tsx
@@ -10,12 +10,16 @@ const Header: NextPage = () => {
     const pathname = usePathname();
 
     return (
-        <header>
-            <nav>
-                <ul>
+        <header className="flex max-w-3xl container mx-auto px-6 py-4">
+            <nav className="flex w-full">
+                <ul className="flex flex-1 ml-[-0.5rem] justify-center space-x-4 md:justify-start md:space-x-0">
                     {HeaderNavLinks?.map((nav : any) => (
-                        <li key={nav?.title}>
-                            <Link href={nav?.href}>
+                        <li key={nav?.title} className="p-2">
+                            <Link className={`flex ${
+                                pathname === nav?.href
+                                    ? "underline"
+                                    : "dark:text-slate-500 outline-none hover:underline dark:hover:text-slate-200 "
+                            }`} href={nav?.href}>
                                 {nav?.title}
                             </Link>
                         </li>
