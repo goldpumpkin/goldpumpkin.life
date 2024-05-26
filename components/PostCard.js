622193821bf53aec2b5f9ad1e1c4d1da62d6acd3
gold <goldhuang24@gmail.com>
2024-05-27 00:20:45 +0800

    fix blog route

--- components/PostCard.js
+++ components/PostCard.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function PostCard(props) {
     const { post } = props
     return (
-        <Link className="unstyled" href={`/content/posts/${post.slug}`}>
+        <Link className="unstyled" href={`/blog/${post.slug}`}>
             <div className="flex justify-between content-center p-4 mb-4 border hover:shadow-sm dark:hover:bg-slate-900 hover:bg-slate-50 dark:border-slate-600 dark:bg-black dark:hover:shadow-sm">
                 <h3>{post.title}</h3>
                 <time className="text-sm text-slate-400 mt-2">
@@ -12,6 +12,4 @@ export default function PostCard(props) {
             </div>
         </Link>
     )
-
-
 }
