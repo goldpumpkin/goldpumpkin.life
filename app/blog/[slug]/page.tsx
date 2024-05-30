97b0907e971e66a41c83f358bfeed5b180a1674b
gold <goldhuang24@gmail.com>
2024-05-31 00:27:39 +0800

    feat: add title on blog page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,15 +1,13 @@
-import Markdown from "markdown-to-jsx"
 import React from 'react'
 import getPostMetadata from "@/lib/getPostMetadata";
 import getPostContent from "@/lib/getPostContent";
-import Link from "next/link";
 import CustomMDX from "@/components/CustomMDX";
 
 export const dynamicParams = false;
 
 export const generateStaticParams = async () => {
     const posts = getPostMetadata('content/posts')
-    return posts.map((post) => ({ slug: post.slug }))
+    return posts.map((post) => ({ slug: post.slug}))
 }
 
 export async function generateMetadata( params: any, searchParams: any) {
@@ -21,14 +19,18 @@ export async function generateMetadata( params: any, searchParams: any) {
 
 export default function Blog(props: any) {
 
-    const slug = props.params.slug
+    const slug = props.params.slug;
     const decodeSlug = decodeURI(slug);
     const post = getPostContent(decodeSlug)
+    const title = post.data.title;
     return (
-        <main>
-            <article>
-                <CustomMDX content={post.content} />
+            <article className = "flex flex-col items-start justify-center w-full">
+                <h1 className="font-extrabold text-2xl tracking-tight mb-0">
+                    {title}
+                </h1>
+                <div className = "w-full prose dark:prose-invert max-w-none mb-30">
+                    <CustomMDX content={post.content} />
+                </div>
             </article>
-        </main>
     )
 }
